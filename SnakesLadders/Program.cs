a2568319de2653337b1fe03176490cf6b9b846e0
sharyu raut <sharyukraut210298@gmail.com>
2020-09-30 09:03:19 +0530

    [Sharyu] Add.uc1 Single player and start position 0

--- SnakesLadders/Program.cs
+++ SnakesLadders/Program.cs
@@ -7,6 +7,12 @@ namespace SnakesLadders
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snakes and Ladders!");
+
+            //constants
+            int NO_OF_PLAYERS = 1;
+
+            //variables
+            int START_POSITION = 0;
         }
     }
 }
