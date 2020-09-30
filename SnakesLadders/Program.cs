f19beea4378b7c39942db5fb056ed20b54a57c73
sharyu raut <sharyukraut210298@gmail.com>
2020-09-30 09:58:19 +0530

    [Sharyu] Add. uc3 options to play added

--- SnakesLadders/Program.cs
+++ SnakesLadders/Program.cs
@@ -4,20 +4,43 @@ namespace SnakesLadders
 {
     class Program
     {
+        //constants
+        public const int NO_OF_PLAYERS = 1;
+        public const int LADDER = 1;
+        public const int SNAKE = 2;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snakes and Ladders!");
 
-            //constants
-            int NO_OF_PLAYERS = 1;
-
             //variables
             int START_POSITION = 0;
+            int CURRENT_POSITION = 0;
 
             Random random = new Random();
 
             int die = random.Next(1, 7);
             Console.WriteLine("Rolling Die: " + die);
+
+            int option = random.Next(0, 3);
+
+            switch (option)
+            {
+                case LADDER:
+                    CURRENT_POSITION += die;
+                    Console.WriteLine("Ladder");
+                    break;
+
+                case SNAKE:
+                    CURRENT_POSITION -= die;
+                    Console.WriteLine("Snake");
+                    break;
+
+                default:
+                    Console.WriteLine("No Play");
+                    break;
+            }
+            Console.WriteLine("Your new position is: " + CURRENT_POSITION);
         }
     }
 }
